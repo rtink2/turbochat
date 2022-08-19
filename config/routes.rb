8efbc8fe84dadc29933464e38fe63b7e5816ce61
rtink2 <tinkham0408@gmail.com>
2022-08-18 20:21:52 -0400

    added rooms & messages

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  resources :rooms do
+    resources :messages
+  end
+  root 'pages#home'
+  devise_for :users
+  
+  devise_scope :user do
+    get 'users', to: 'devise/sessions#new'
+  end
+
+  get 'user/:id', to: 'users#show', as: 'user'
+  
+end
