38bdbff075cd2147b7576a31f3ee9feecbbb28cb
rtink2 <tinkham0408@gmail.com>
2022-08-24 22:12:35 -0400

    added image, video, audio feature with previews

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,6 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_status
-
   def index
     @room = Room.new
     @rooms = Room.public_rooms
@@ -28,9 +27,8 @@ class RoomsController < ApplicationController
   end
 
   private
-  
+
   def set_status
     current_user.update!(status: User.statuses[:online]) if current_user
   end
-
 end
